59a0e5760902e031ecc337816404599a429d61de
RodolfoChavero79 <106353627+RodolfoChavero79@users.noreply.github.com>
2023-03-10 00:17:54 -0600

    Ajuste deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://RodolfoChavero79/.github.io/myRepository/",
+  base: "https://github.com/RodolfoChavero79/pruebaFront",
   plugins: [react()],
 })
